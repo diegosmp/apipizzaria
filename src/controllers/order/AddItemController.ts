34982652c13974f37d19ddcbc5d95811a410b830
diegosmp <diiegosmp@gmail.com>
2024-09-21 09:13:37 -0300

    project pizzaria back end complete

---
+++ src/controllers/order/AddItemController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { AddItemService } from '../../services/order/AddItemService'
+
+class AddItemController {
+   async handle(req: Request, res: Response) {
+      const { order_id, product_id, amount } = req.body
+
+      const addItemSevice = new AddItemService()
+      const item = await addItemSevice.execute({ order_id, product_id, amount })
+
+      return res.json(item)
+   }
+}
+
+export { AddItemController }
